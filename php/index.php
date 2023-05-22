169623768d5a87ad2b44d2814e280bbb33356604
Trương Văn Quang <truongvanquang@Quang-Trng.local>
2023-05-22 16:09:07 +0700

    done

---
+++ php/index.php
@@ -0,0 +1,15 @@
+
+
+<?php
+const URL_BASE = "http://alooo.com";
+define("WELCOME","HELLO");
+$x = 5;
+$str = "van quang";
+echo $x.$str;
+echo WELCOME;
+// $arr = array(
+//     1,4,6,7,8
+// );
+// echo sizeof($arr);
+// echo count($arr);
+// echo $arr[3];
